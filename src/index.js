5ae40852ff238dc30a59d1b521cd837e4b3bbfcb
esra tansuk <esra.balkis@outlook.com>
2022-11-22 21:16:43 +0300

    transform

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./scss/main.scss";
+// import * as bootstrap from "bootstrap";
+import Dropdown from "bootstrap/js/dist/dropdown";
+import Collapse from "bootstrap/js/dist/collapse";
+import Tab from "bootstrap/js/dist/tab";
+console.log("Hello World!");
